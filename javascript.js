f41d4b203033361e0508b3af0bed027fc194e657
islam12zr <99747255+islam12zr@users.noreply.github.com>
2022-02-26 01:50:28 +1200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,62 @@
+let stars1 = document.getElementById ('stars1');
+let moon2 = document.getElementById ('moon');
+let mountains3 = document.getElementById ('mountains3');
+let mountains4 = document.getElementById ('mountains4');
+let river5 = document.getElementById ('river5');
+let boat6 = document.getElementById ('boat6');
+let ISLAM = document.querySelector ( '.ISLAM');
+alert("islam");
+window.onscroll = function(){
+let value = scrollY;
+console.log(value);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
